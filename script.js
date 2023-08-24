885e78cb11b29d050795288144e688bfde4b3ce5
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-08-24 14:36:17 -0500

    Add when button is clicked array changes read value

--- script.js
+++ script.js
@@ -18,27 +18,57 @@ function addBookToLibrary(){
 function displayLibrary(library){ 
   const displayLibrary = document.querySelector('.library');
     let size = library.length - 1;
+
     const book = document.createElement('div');
     book.classList = `${library[size].title}`; //add to allow removing
-    book.innerText = `${library[size].author}, ${library[size].title}, ${library[size].pages}, ${library[size].read}`;
+    book.innerText = `Author: ${library[size].author} \nTitle: ${library[size].title} \nPages: ${library[size].pages} \nRead: ${library[size].read}`;
+    
+    const status = document.createElement('button');
+    status.classList.add('btnStatus');
+    status.innerText = 'Change Read Status'
+
     const btnRemove = document.createElement('button');
     btnRemove.classList.add("btnRemove");
     btnRemove.innerText = 'X';
+
+    book.appendChild(status);
     book.appendChild(btnRemove);
     displayLibrary.appendChild(book);
+
     remove(library[size].title);
+    changeRead(library[size].title);
+}
+
+//Change the read status of the book
+function changeRead(pos){
+  document.querySelectorAll('.library > div > .btnStatus').forEach(read =>{
+    read.addEventListener('click', (e)=>{
+      const objIndex = myLibrary.findIndex((obj) => obj.title === pos);
+      if(objIndex > -1){
+        const status = myLibrary[objIndex].read;
+        if(status === 'Yes'){
+          myLibrary[objIndex].read = 'No';
+          displayLibrary(myLibrary[objIndex]);
+        }
+        else{
+          myLibrary[objIndex].read = 'Yes';
+          displayLibrary(myLibrary[objIndex]);
+        }
+      }
+    });
+  });
 }
 
 function remove(pos){ //on button click removes from myLibrary and user interface
-  document.querySelectorAll('.library > div > button').forEach(userBook => {
+  document.querySelectorAll('.library > div > .btnRemove').forEach(userBook => {
     userBook.addEventListener('click', (e)=>{
       e.target.parentNode.remove();
       const objIndex = myLibrary.findIndex((obj) => obj.title === pos);
       if(objIndex > -1){
         myLibrary.splice(objIndex,1);
       }
-    })
-  })
+    });
+  });
 }
 
 let myLibrary = [];
